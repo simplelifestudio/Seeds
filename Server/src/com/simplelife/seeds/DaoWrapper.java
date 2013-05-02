4424dbfdffc934875b8bef846438a80b4aa67ca0
Tomas <simplelife.tomas@gmail.com>
2013-05-03 07:24:07 +0800

    为了编译通过，注释hibernate库

--- Server/src/com/simplelife/seeds/DaoWrapper.java
+++ Server/src/com/simplelife/seeds/DaoWrapper.java
@@ -1,11 +1,13 @@
 package com.simplelife.seeds;
+
+/*
 import org.hibernate.Hibernate;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-
+*/
 
 public class DaoWrapper {
 
