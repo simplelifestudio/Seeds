6b22dd46b0ca047ce153a5d802f9c16307394636
Tomas <simplelife.tomas@gmail.com>
2013-05-03 07:22:05 +0800

    改名

--- Server/src/com/simplelife/seeds/JsonCommandInterface.java
+++
@@ -1,8 +0,0 @@
-package com.simplelife.seeds;
-
-import net.sf.json.JSONObject;
-import java.io.PrintWriter;
-
-public interface JsonCommandInterface {
-	public void Execute(JSONObject jsonObj, PrintWriter out);
-}
