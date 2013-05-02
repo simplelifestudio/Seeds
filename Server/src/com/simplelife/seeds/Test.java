e32f46466aa47689454d0e89ddcf2bc61c3003de
Tomas <simplelife.tomas@gmail.com>
2013-05-03 07:22:57 +0800

    测试类

---
+++ Server/src/com/simplelife/seeds/Test.java
@@ -0,0 +1,10 @@
+package com.simplelife.seeds;
+
+public class Test {
+	public static void main(String[] args)
+	{
+		HtmlParser parser = new HtmlParser();
+		parser.Parse();
+		//parser.dateNeedParse("[3-15]aaaa");
+	}
+}
