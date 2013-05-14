3e9fa29e44cc9c02fa2ccdaa21301c28f977b80d
Patrick <simplife.patrick@gmail.com>
2013-05-14 21:03:24 +0800

    清理无用的注释

--- iOS/Seeds/Seeds/SeedDAOFmdbImpl.m
+++ iOS/Seeds/Seeds/SeedDAOFmdbImpl.m
@@ -128,10 +128,8 @@
              {
                  [sql appendString:key];
                  [sql appendString:@" = "];
-//                 [sql appendString:@"'"];
                  [sql appendString:@":"];
                  [sql appendString:key];
-//                 [sql appendString:@"'"];
                  if (1 < paramCount)
                  {
                      [sql appendString:@", "];
@@ -256,58 +254,7 @@
 -(BOOL) insertSeed:(Seed *)seed
 {
     __block BOOL flag = NO;
-    
-//    [databaseQueue inDatabase:^(FMDatabase* db)
-//    {
-//        [db open];
-//        
-//        if (nil != seed)
-//        {
-//            NSMutableString* sql = [NSMutableString stringWithString:@"insert into "];
-//            [sql appendString:TABLE_SEED];
-//            [sql appendString:@" ("];
-//            [sql appendString:TABLE_SEED_COLUMN_TYPE];
-//            [sql appendString:@", "];
-//            [sql appendString:TABLE_SEED_COLUMN_SOURCE];
-//            [sql appendString:@", "];
-//            [sql appendString:TABLE_SEED_COLUMN_PUBLISHDATE];
-//            [sql appendString:@", "];
-//            [sql appendString:TABLE_SEED_COLUMN_NAME];
-//            [sql appendString:@", "];
-//            [sql appendString:TABLE_SEED_COLUMN_SIZE];
-//            [sql appendString:@", "];
-//            [sql appendString:TABLE_SEED_COLUMN_FORMAT];
-//            [sql appendString:@", "];
-//            [sql appendString:TABLE_SEED_COLUMN_TORRENTLINK];
-//            [sql appendString:@", "];
-//            [sql appendString:TABLE_SEED_COLUMN_FAVORITE];
-//            [sql appendString:@", "];
-//            [sql appendString:TABLE_SEED_COLUMN_MOSAIC];
-//            [sql appendString:@", "];
-//            [sql appendString:TABLE_SEED_COLUMN_HASH];
-//            [sql appendString:@", "];
-//            [sql appendString:TABLE_SEED_COLUMN_MEMO];
-//            [sql appendString:@") values ("];
-//            [sql appendString:@"?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?"];
-//            [sql appendString:@")"];
-//            
-//            BOOL hadError = [db hadError];
-//            
-//            flag = [db executeUpdate:sql, seed.type, seed.source, seed.publishDate, seed.name, seed.size, seed.format, seed.torrentLink, (seed.favorite) ? @"1" : @"0", (seed.mosaic) ? @"1" : @"0", seed.hash, seed.memo];
-//            hadError = [db hadError];
-//            if (hadError)
-//            {
-//                NSLog(@"FMDatabase error %d: %@", [db lastErrorCode], [db lastErrorMessage]);
-//            }
-//            if (!flag)
-//            {
-//                DLog(@"Fail to insert seed record with name: %@", seed.name);
-//            }
-//        }
-//        
-//        [db close];
-//    }];
-    
+        
     [databaseQueue inTransaction:^(FMDatabase* db, BOOL* rollback)
     {
         [db open];
@@ -373,7 +320,7 @@
                         {
                             if (nil != picture)
                             {
-                                // Can't use NSInteger or int here as FMDB issue
+                                // NOTE: Can't use NSInteger or int here as FMDB issue
                                 NSNumber* oSeedId = [NSNumber numberWithInteger:maxSeedId];
                                 
                                 NSMutableString* sql = [NSMutableString stringWithString:@"insert into "];
