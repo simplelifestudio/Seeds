d1bf5ea5fa6ce28975cfcb165676f9dbeccd2406
Patrick <simplife.patrick@gmail.com>
2013-05-19 00:01:15 +0800

    加入Objective-Zip的压缩包创建测试代码

--- iOS/Seeds/Seeds/CBFileUtils.m
+++ iOS/Seeds/Seeds/CBFileUtils.m
@@ -10,6 +10,26 @@
 
 @implementation CBFileUtils
 
+/*
+ NSString *documentsDir = [CBPathUtils documentsDirectoryPath];
+ 
+ NSString* torrentFullPath = [documentsDir stringByAppendingPathComponent:@"TDIsOck5la.torrent"];
+ 
+ NSString *filePath = [documentsDir stringByAppendingPathComponent:@"test.zip"];
+ 
+ ZipFile *zipFile = [[ZipFile alloc] initWithFileName:filePath mode:ZipFileModeCreate];
+ 
+ //向当前Zip文件中添加文件需要使用ZipFileModeAppend模式
+ ZipWriteStream *writeStream = [zipFile writeFileInZipWithName:@"TDIsOck5la.torrent" compressionLevel:ZipCompressionLevelNone];
+ //yourfilename是存入的文件名
+ //compressionLevel指示压缩率级别，可以选择ZipCompressionLevelFast(最快), ZipCompressionLevelBest(最大压缩率)，ZipCompressionLevelNone(不压缩)
+ //如果使用密码和CRC校验可以使用另外的写入函数
+ NSData* data = [CBFileUtils dataFromFile:torrentFullPath];
+ [writeStream writeData:data];
+ //data是需要压入的文件内容(NSData类型)
+ [writeStream finishedWriting];
+ [zipFile close];
+ */
 +(NSData*) dataFromFile:(NSString *)filePath
 {
     NSData *data = nil;
