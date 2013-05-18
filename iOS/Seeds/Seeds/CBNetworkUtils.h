61b8297348997486e91774fbbcea16fa2b330267
Patrick <simplife.patrick@gmail.com>
2013-05-18 19:27:47 +0800

    新增网络工具类

---
+++ iOS/Seeds/Seeds/CBNetworkUtils.h
@@ -0,0 +1,15 @@
+//
+//  CBNetworkUtils.h
+//  Seeds
+//
+//  Created by Patrick Deng on 13-5-18.
+//  Copyright (c) 2013年 SimpleLife Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CBNetworkUtils : NSObject
+
++(NSString*) hostName;
+
+@end
