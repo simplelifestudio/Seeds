4b402bed05c96ad0a67b22b3d4fd6cabed59c840
Patrick <simplife.patrick@gmail.com>
2013-05-19 09:04:09 +0800

    传输界面增加控制台视图

--- iOS/Seeds/Seeds/TransmitViewController.m
+++ iOS/Seeds/Seeds/TransmitViewController.m
@@ -16,6 +16,7 @@
 
 @synthesize addressLabel = _addressLabel;
 @synthesize statusLabel = _statusLabel;
+@synthesize consoleView = _consoleView;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -83,6 +84,7 @@
 {
     [self setAddressLabel:nil];
     [self setStatusLabel:nil];
+    [self setConsoleView:nil];
     [super viewDidUnload];
 }
 @end
