9371606b59fc065a44c46b3497de418f93d6cac0
Tomas <simplelife.tomas@gmail.com>
2013-06-01 21:55:07 +0800

    Add log of response

--- Server/src/com/simplelife/seeds/server/JsonCommandAlohaReq.java
+++ Server/src/com/simplelife/seeds/server/JsonCommandAlohaReq.java
@@ -1,14 +1,21 @@
 package com.simplelife.seeds.server;
 
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import net.sf.json.JSONObject;
 
 public class JsonCommandAlohaReq extends JsonCommandBase {
+	private Logger logger = Logger.getLogger("HtmlParser");
+	
 	@Override
 	public void Execute(JSONObject jsonObj, PrintWriter out) {
+		
 		String res ="{\"command\": \"AlohaResponse\", \"paramList\": {\"content\":\"Hello Seeds App![";
 		res += DateUtil.getNowDate().toString();
 		res +="]\"}}";
 		out.println(res);
+		logger.log(Level.INFO, "Aloha response: \n" + res);
 	}
 }
