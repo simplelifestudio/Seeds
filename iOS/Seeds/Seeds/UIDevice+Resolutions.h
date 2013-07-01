9c3e42ab31601e28b07f3ecbb9107e6cfb44542d
Patrick <simplife.patrick@gmail.com>
2013-07-01 21:13:00 +0800

    新增设备分辨率检测代码

---
+++ iOS/Seeds/Seeds/UIDevice+Resolutions.h
@@ -0,0 +1,40 @@
+//
+//  UIDevice+Resolutions.h
+//  Seeds
+//
+//  Created by Patrick Deng on 13-7-1.
+//  Copyright (c) 2013年 SimpleLife Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <UIKit/UIKit.h>
+
+enum
+{
+    // iPhone 1,3,3GS 标准分辨率(320x480px)
+    UIDevice_iPhoneStandardRes      = 1,
+    // iPhone 4,4S 高清分辨率(640x960px)
+    UIDevice_iPhoneHiRes            = 2,
+    // iPhone 5 高清分辨率(640x1136px)
+    UIDevice_iPhoneTallerHiRes      = 3,
+    // iPad 1,2 标准分辨率(1024x768px)
+    UIDevice_iPadStandardRes        = 4,
+    // iPad 3 High Resolution(2048x1536px)
+    UIDevice_iPadHiRes              = 5
+};
+
+typedef NSUInteger UIDeviceResolution;
+
+@interface UIDevice (Resolutions)
+{
+    
+}
+
++ (UIDeviceResolution) currentResolution;
+
++ (BOOL)isRunningOniPhone5;
+
++ (BOOL)isRunningOniPhone;
+
+@end
---
+++ iOS/Seeds/Seeds/UIDevice+Resolutions.m
@@ -0,0 +1,54 @@
+//
+//  UIDevice+Resolutions.m
+//  Seeds
+//
+//  Created by Patrick Deng on 13-7-1.
+//  Copyright (c) 2013年 SimpleLife Studio. All rights reserved.
+//
+
+#import "UIDevice+Resolutions.h"
+
+@implementation UIDevice (Resolutions)
+
++ (UIDeviceResolution) currentResolution
+{
+    if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+    {
+        if ([[UIScreen mainScreen] respondsToSelector: @selector(scale)])
+        {
+            CGSize result = [[UIScreen mainScreen] bounds].size;
+            result = CGSizeMake(result.width * [UIScreen mainScreen].scale, result.height * [UIScreen mainScreen].scale);
+            if (result.height <= 480.0f)
+            {
+                return UIDevice_iPhoneStandardRes;
+            }
+            
+            return (result.height > 960 ? UIDevice_iPhoneTallerHiRes : UIDevice_iPhoneHiRes);
+        }
+        else
+        {
+            return UIDevice_iPhoneStandardRes;
+        }
+    }
+    else
+    {
+        return (([[UIScreen mainScreen] respondsToSelector: @selector(scale)]) ? UIDevice_iPadHiRes : UIDevice_iPadStandardRes);
+    }
+}
+
++ (BOOL)isRunningOniPhone5
+{
+    if ([self currentResolution] == UIDevice_iPhoneTallerHiRes)
+    {
+        return YES;
+    }
+    
+    return NO;
+}
+
++ (BOOL)isRunningOniPhone
+{
+    return (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone);
+}
+
+@end
