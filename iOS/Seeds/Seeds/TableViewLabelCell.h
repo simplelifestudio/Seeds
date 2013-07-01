89bdaba671e6e4bd48bd9249441ff953ac297140
Patrick <simplife.patrick@gmail.com>
2013-07-01 19:38:07 +0800

    新增TableViewCell自定义控件

---
+++ iOS/Seeds/Seeds/TableViewLabelCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewLabelCell.h
+//  Seeds
+//
+//  Created by Patrick Deng on 13-7-1.
+//  Copyright (c) 2013年 SimpleLife Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewLabelCell : UITableViewCell
+
+@end
---
+++ iOS/Seeds/Seeds/TableViewLabelCell.m
@@ -0,0 +1,29 @@
+//
+//  TableViewLabelCell.m
+//  Seeds
+//
+//  Created by Patrick Deng on 13-7-1.
+//  Copyright (c) 2013年 SimpleLife Studio. All rights reserved.
+//
+
+#import "TableViewLabelCell.h"
+
+@implementation TableViewLabelCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
