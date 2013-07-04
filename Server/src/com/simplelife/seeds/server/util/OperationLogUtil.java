519017ea394fd1f7a9b6f5c0f394259b049d9934
Tomas <simplelife.tomas@gmail.com>
2013-07-04 23:05:11 +0800

    修改表名称

--- Server/src/com/simplelife/seeds/server/util/OperationLogUtil.java
+++ Server/src/com/simplelife/seeds/server/util/OperationLogUtil.java
@@ -10,36 +10,42 @@
 
 package com.simplelife.seeds.server.util;
 
-import com.simplelife.seeds.server.db.LogId;
 import com.simplelife.seeds.server.db.OperationLog;
 
 public class OperationLogUtil {
 	public static void captureTaskStarted(String logInfo)
 	{
-		save(LogId.SEED_CAP_TASK_START, logInfo);
+		save(OperationCode.SEED_CAP_TASK_START, logInfo);
 	}
 	
 	public static void captureTaskSucceed(String logInfo)
 	{
-		save(LogId.SEED_CAP_TASK_SUCCEED, logInfo);
+		save(OperationCode.SEED_CAP_TASK_SUCCEED, logInfo);
 	}
 	
 	public static void captureTaskFailed(String logInfo)
 	{
-		save(LogId.SEED_CAP_TASK_FAILED, logInfo);
+		save(OperationCode.SEED_CAP_TASK_FAILED, logInfo);
 	}
 	
 	
 	public static void save(long logId, String logInfo)
 	{
-		OperationLog log = new OperationLog(logId, logInfo);
-		if (logId == 0)
+		try
 		{
-			LogUtil.severe("logId can't be 0.");
-			return;
+			OperationLog log = new OperationLog(logId, logInfo);
+			if (logId == 0)
+			{
+				LogUtil.severe("logId can't be 0.");
+				return;
+			}
+				
+			log.setLogDate(DateUtil.getNow());
+			DaoWrapper.save(log);
 		}
-			
-		log.setLogDate(DateUtil.getNow());
-		DaoWrapper.save(log);
+		catch (Exception e)
+		{
+			LogUtil.printStackTrace(e);
+		}
 	}
 }
