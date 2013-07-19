780d24cf85455cb0f1bcb94fa467037776b4729a
Tomas <simplelife.tomas@gmail.com>
2013-07-19 08:46:41 +0800

    多处改动如下：
    1. 即使所有seedId失败也返回JsonResponseSeedsToCart   #31
    2. 优化数据库失败时的错误提示
    3. 增加重复订阅更新订阅日期的操作   #29
    4. 增加订阅日期字段   #29

--- Server/src/com/simplelife/seeds/server/json/JsonResponseSeedsToCart.java
+++ Server/src/com/simplelife/seeds/server/json/JsonResponseSeedsToCart.java
@@ -16,6 +16,7 @@ import java.util.List;
 import com.simplelife.seeds.server.json.JsonRequestSeedsToCart.SeedToCartResult;
 import com.simplelife.seeds.server.util.DBExistResult;
 import com.simplelife.seeds.server.util.DaoWrapper;
+import com.simplelife.seeds.server.util.DateUtil;
 import com.simplelife.seeds.server.util.ErrorCode;
 import com.simplelife.seeds.server.util.JsonKey;
 import com.simplelife.seeds.server.util.LogUtil;
@@ -86,7 +87,11 @@ public class JsonResponseSeedsToCart extends JsonResponseBase
     {
         if (seedList == null || seedList.size() == 0)
         {
-            LogUtil.severe("seedList is null or empty");
+        	List<String> successList = new ArrayList<String>();
+            body.put(JsonKey.cartId, this.cartId);
+            body.put(JsonKey.successSeedIdList, successList);
+            body.put(JsonKey.failedSeedIdList, successList);
+            outPrintWriter.write(toString());
             return;
         }
         
@@ -115,16 +120,11 @@ public class JsonResponseSeedsToCart extends JsonResponseBase
             }
         }
         
-        if (successList.size() == 0)
+        if ((successList.size() == 0) && (result == SeedToCartResult.failed))
         {
-            if (result == SeedToCartResult.invalid)
-            {
-                responseError(ErrorCode.IllegalMessageBody, "Invalid seedId.");
-            }
-            else if (result == SeedToCartResult.failed)
-            {
-                responseError(ErrorCode.DatabaseConnectionError, "Unknown error on DB connection.");
-            }
+        	// If all seedId failed, change response from JsonResponseSeedsToCart to ErrorResponse
+        	response.remove(JsonKey.id);
+            responseError(ErrorCode.DatabaseConnectionError, "Unknown error on DB connection.");
             return;
         }
         
@@ -137,24 +137,39 @@ public class JsonResponseSeedsToCart extends JsonResponseBase
     private SeedToCartResult saveRssRequest(String seedId, String cartId)
     {
         String sql = "select " + TableColumnName.seedId + " from " + TableName.Seed + " where " + TableColumnName.seedId + " = " + seedId;
-        if (DaoWrapper.exists(sql) != DBExistResult.existent) {
+        DBExistResult result = DaoWrapper.exists(sql);
+        
+        if (result == DBExistResult.errorOccurred)
+        {
+            LogUtil.severe("Error on DB connection.");
+            return SeedToCartResult.failed;
+        }
+        
+        if (result != DBExistResult.existent) {
             LogUtil.severe("Invalid seedId which can't be found in DB: " + seedId);
             return SeedToCartResult.invalid;
         }
 
         sql = "select " + TableColumnName.seedId + " from " + TableName.Cart + " where " + TableColumnName.cartId + " = '" + cartId + "' and " + TableColumnName.seedId +" = " + seedId;
-        DBExistResult result = DaoWrapper.exists(sql);
+        result = DaoWrapper.exists(sql);
         if (result == DBExistResult.existent) {
             LogUtil.warning("Duplicated request received, seed <" + seedId + "> has been subscribed by " + cartId);
+            sql = "update "  + TableName.Cart  
+                    + " set " + TableColumnName.date + " = '" + DateUtil.getToday() + "' where "
+                    + TableColumnName.cartId + " = '" + cartId + "' and "
+                    + TableColumnName.seedId + " = " + seedId+ "";
+            DaoWrapper.executeSql(sql);
             return SeedToCartResult.duplicated;
         }
         else if (result == DBExistResult.errorOccurred)
         {
-            LogUtil.info("Abnormal DB status, schedule next task tomorrow.");
+            LogUtil.info("Abnormal DB status, check DB setting.");
             return SeedToCartResult.failed;
         }
 
-        sql = "Insert into " + TableName.Cart +"("+ TableColumnName.cartId + ", "+ TableColumnName.seedId +") " + "values ('" + cartId + "', " + seedId + ")";
+        sql = "Insert into " + TableName.Cart 
+                +"("+ TableColumnName.cartId + ", " + TableColumnName.seedId  + ", " + TableColumnName.date +") "
+                + "values ('" + cartId + "', " + seedId + ",'" + DateUtil.getToday() + "')";
         if (DaoWrapper.executeSql(sql))
         {
             LogUtil.info("Succeed to subscribe seed <" + seedId + "> for " + cartId);
