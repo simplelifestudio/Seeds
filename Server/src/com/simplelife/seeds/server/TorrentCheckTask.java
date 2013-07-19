2b4a3e10ed3cbae842e84f5a520b81c8140199e8
Tomas <simplelife.tomas@gmail.com>
2013-07-19 08:53:44 +0800

    过期种子检查任务
    包括检查数据库中的过期订阅和已经下载的过期种子

---
+++ Server/src/com/simplelife/seeds/server/TorrentCheckTask.java
@@ -0,0 +1,88 @@
+/**
+ * TorrentCheckTask.java
+ * 
+ * History:
+ *     2013-7-18: Tomas Chen, initial version
+ * 
+ * Copyright (c) 2013 SimpleLife Studio. All rights reserved.
+ */
+
+package com.simplelife.seeds.server;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Date;
+import java.util.TimerTask;
+
+import com.simplelife.seeds.server.util.DBExistResult;
+import com.simplelife.seeds.server.util.DaoWrapper;
+import com.simplelife.seeds.server.util.DateUtil;
+import com.simplelife.seeds.server.util.GlobalSetting;
+import com.simplelife.seeds.server.util.HttpUtil;
+import com.simplelife.seeds.server.util.LogUtil;
+import com.simplelife.seeds.server.util.TableColumnName;
+import com.simplelife.seeds.server.util.TableName;
+
+/**
+ * 
+ */
+public class TorrentCheckTask extends TimerTask
+{
+    @Override
+    public void run()
+    {
+        checkExpiredRssInDb();
+        checkExpiredTorrentFile();
+    }
+    
+    public void checkExpiredRssInDb()
+    {
+        String deleteDate = DateUtil.getDateStringByDayBack(GlobalSetting.torrentExpireDays);
+        String sql = "delete from " + TableName.Cart + " where " + TableColumnName.date + " <= '" + deleteDate + "'";
+        DaoWrapper.executeSql(sql);
+    }
+    
+    public void checkExpiredTorrentFile()
+    {
+        LogUtil.info("Start to check expired torrent file.");
+        String strPath = HttpUtil.getSeedSavePath();
+        File seedPath = new File(strPath);
+        
+        if (!seedPath.exists())
+        {
+            LogUtil.warning("Seed save path is not existent: " + strPath);
+            return;
+        }
+        
+        if (!seedPath.isDirectory())
+        {
+            LogUtil.severe("Seed save path is invalid: " + strPath);
+            return;
+        }
+        
+        checkAndDeleteFile(seedPath);
+        LogUtil.info("Finished check expired torrent file.");
+    }
+    
+    
+    public void checkAndDeleteFile(File seedPath)
+    {
+        File[] torrentFiles = seedPath.listFiles();
+        File torrentFile;
+        Date date;
+        long dayDiff;
+        for (int i = 0; i < torrentFiles.length; i++)
+        {
+            torrentFile = torrentFiles[i];
+            if (torrentFile.getName().endsWith(".torrent"))
+            {
+                date = new Date(torrentFile.lastModified());
+                dayDiff = DateUtil.getDaysFromToday(date); 
+                if (dayDiff < -GlobalSetting.torrentExpireDays)
+                {
+                    torrentFile.delete();
+                }
+            }
+        }
+    }
+}
