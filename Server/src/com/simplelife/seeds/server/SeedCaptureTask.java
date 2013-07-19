01b879d6d6e1061ba3e211b020edfed4c264305a
Tomas <simplelife.tomas@gmail.com>
2013-07-19 08:30:52 +0800

    增加抓取任务的互斥操作
    以避免数据库重复数据的问题（同时启动的抓取任务会绕过数据库中的抓取任务状态标志位）
    #23

--- Server/src/com/simplelife/seeds/server/SeedCaptureTask.java
+++ Server/src/com/simplelife/seeds/server/SeedCaptureTask.java
@@ -11,20 +11,22 @@
 package com.simplelife.seeds.server;
 
 import java.util.TimerTask;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import com.simplelife.seeds.server.parser.HtmlParser;
 import com.simplelife.seeds.server.util.LogUtil;
 
 public class SeedCaptureTask extends TimerTask {
-	
+    private final static Lock l = new ReentrantLock();
+    
 	@Override
 	public void run() {
 		LogUtil.info("Seed capture task launched.");
+		l.lock();
 		HtmlParser parser = new HtmlParser();
 		parser.Parse();
-		
 		LogUtil.info("Seed capture task finished.");
-		//SeedCaptureListener.scheduleNextCapture();
+		l.unlock();
 	}
-
 }
