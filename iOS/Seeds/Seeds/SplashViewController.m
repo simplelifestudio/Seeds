734aced1716f4000eb5a680723362dfc2bf1aff4
Patrick <simplife.patrick@gmail.com>
2013-07-22 14:37:13 +0800

    修正多语言显示

--- iOS/Seeds/Seeds/SplashViewController.m
+++ iOS/Seeds/Seeds/SplashViewController.m
@@ -76,7 +76,7 @@
         [warningVC setAgreeButtonVisible:NO];
         [warningVC setDeclineButtonVisible:NO];
         [warningVC setCountdownSeconds:WARNING_DISPLAY_SECONDS];
-        [warningVC setWarningText:NSLocalizedString(@"Currently Seeds iOS app supports iPhone5 only, sorry for inconvenience. App will be terminated automatically once countdown is end.", nil)];
+        [warningVC setWarningText:NSLocalizedString(@"Warning of Unsupported Devices", nil)];
         
         return;
     }
