c80ad22a8c0146a2879d65fd189cdd36d3755926
simplelife-patrick <simplelife.patrick@gmail.com>
2013-08-14 20:02:06 +0800

    新增GZip工具类

---
+++ iOS/Seeds/Seeds/CBGZipUtils.h
@@ -0,0 +1,17 @@
+//
+//  CBGZipUtils.h
+//  Seeds
+//
+//  Created by Patrick Deng on 13-8-14.
+//  Copyright (c) 2013年 SimpleLife Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "zlib.h"
+
+@interface CBGZipUtils : NSObject
+
++(NSData*) gzipData:(NSData *)pUncompressedData;
++(NSData*) uncompressZippedData:(NSData *)compressedData;
+
+@end
